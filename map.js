f36eefac2ea1d6b44ec14280974c80876302cc8d
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-05-16 16:52:01 +0300

    feat: add correct return

--- map.js
+++ map.js
@@ -1,11 +1,27 @@
 function findShort(q){
-let con=new Map()
-for(let i=0;i<=q.length-1;i++){
-  if (con.has(q[i])==false){
-    con.set(q[i],1)
-}else{
-  con.set(q[i],con.get(q[i])+1)
-}
-}
-return con
-}
+    let con=new Map()
+    for(let i=0;i<=q.length-1;i++){
+      if (con.has(q[i])==false){
+        con.set(q[i],1)
+    }else{
+      con.set(q[i],con.get(q[i])+1)
+    }
+    }
+    let count=[]
+    let i=0
+    for (let t of con.values()) { 
+      count[i]=t
+      i++
+  }
+    let simvol=[]
+    i=0
+    for (let t of con.keys()) { 
+      simvol[i]=t
+      i++
+  }
+    let res=''
+    for(i=0;i<simvol.length;i++){
+      res=res+count[i]+simvol[i]
+    }
+    return res
+  }
